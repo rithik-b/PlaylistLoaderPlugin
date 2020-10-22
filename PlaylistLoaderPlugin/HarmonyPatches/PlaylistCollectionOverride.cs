97fcae0af22e66eaeb6942b1d48b02286fa6115e
rithik bhatia <rithikbhatia12@gmail.com>
2020-10-22 01:06:08 -0400

    - Cleaned up after view changes - Added scrolling functionality with animation
    ToDo:
    - Fix overflowing playlists
    - Fix hitboxes (add pngs as assets)

--- PlaylistLoaderPlugin/HarmonyPatches/PlaylistCollectionOverride.cs
+++ PlaylistLoaderPlugin/HarmonyPatches/PlaylistCollectionOverride.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
 using HMUI;
 using IPA;
@@ -7,24 +8,15 @@ using PlaylistLoaderLite.Objects;
 using SongCore.OverrideClasses;
 using UnityEngine;
 
-/// <summary>
-/// See https://github.com/pardeike/Harmony/wiki for a full reference on Harmony.
-/// </summary>
 namespace PlaylistLoaderLite.HarmonyPatches
 {
-    /// <summary>
-    /// This is a patch of the method <see cref="PlaylistsViewController.SetData(IAnnotatedBeatmapLevelCollection[], int, bool)"/>
-    /// TODO: Remove this or replace it with your own.
-    /// </summary>
     [HarmonyPatch(typeof(AnnotatedBeatmapLevelCollectionsViewController), "SetData",
-        new Type[] { // Specify the types of SetDataFromLevelAsync's parameters here.
+        new Type[] {
         typeof(IReadOnlyList<IAnnotatedBeatmapLevelCollection>), typeof(int), typeof(bool)})]
     public class PlaylistCollectionOverride
     {
         private static IAnnotatedBeatmapLevelCollection[] loadedPlaylists;
-        /// <summary>
-        /// Adds this plugin's name to the beginning of the author text in the song list view.
-        /// </summary>
+
         internal static void Prefix(ref IAnnotatedBeatmapLevelCollection[] annotatedBeatmapLevelCollections)
         {
             // Check if annotatedBeatmapLevelCollections is empty (Versus Tab)
@@ -33,24 +25,27 @@ namespace PlaylistLoaderLite.HarmonyPatches
             // Checks if this is the playlists view
             if (annotatedBeatmapLevelCollections[0] is CustomBeatmapLevelPack)
             {
-                IAnnotatedBeatmapLevelCollection[] tempplaylists = new IAnnotatedBeatmapLevelCollection[6]; // 6 will be the playlists per page
+                IAnnotatedBeatmapLevelCollection[] allCustomBeatmapLevelCollections = new IAnnotatedBeatmapLevelCollection[loadedPlaylists.Length + annotatedBeatmapLevelCollections.Length];
 
                 for (int i = 0; i < annotatedBeatmapLevelCollections.Length; i++)
-                {
-                    tempplaylists[i] = annotatedBeatmapLevelCollections[i];
-                }
-                int j = 0;
-                for (int i = annotatedBeatmapLevelCollections.Length; i < 6; i++)
-                {
-                    tempplaylists[i] = loadedPlaylists[j++];
-                }
-                annotatedBeatmapLevelCollections = tempplaylists;
+                    allCustomBeatmapLevelCollections[i] = annotatedBeatmapLevelCollections[i];
 
-                if (NavigationButtons.leftPlaylistsButton == null)
+                int j = 0;
+                for (int i = annotatedBeatmapLevelCollections.Length; i < allCustomBeatmapLevelCollections.Length; i++)
+                    allCustomBeatmapLevelCollections[i] = loadedPlaylists[j++];
+
+                if (allCustomBeatmapLevelCollections.Length > NavigationButtons.MAX_PLAYLISTS_PER_PAGE) // If there are more playlists than max, enable buttons
                 {
-                    NavigationButtons.ButtonsInit();
+                    annotatedBeatmapLevelCollections = allCustomBeatmapLevelCollections;//allCustomBeatmapLevelCollections.Take(NavigationButtons.MAX_PLAYLISTS_PER_PAGE).ToArray();
+                    NavigationButtons.EnableButtons();
                 }
+                else
+                    annotatedBeatmapLevelCollections = allCustomBeatmapLevelCollections;
             }
+            else
+            {
+                NavigationButtons.DisableButtons();
+            }    
         }
 
         public static int RefreshPlaylists()
--- PlaylistLoaderPlugin/Objects/NavigationButtons.cs
+++ PlaylistLoaderPlugin/Objects/NavigationButtons.cs
@@ -10,19 +10,30 @@ namespace PlaylistLoaderLite.Objects
 {
     internal class NavigationButtons
     {
-        public static NoTransitionsButton leftPlaylistsButton;
-        public static NoTransitionsButton rightPlaylistsButton;
+        private static NoTransitionsButton _leftPlaylistsButton;
+        private static NoTransitionsButton _rightPlaylistsButton;
+
+        private static AnnotatedBeatmapLevelCollectionsViewController _annotatedBeatmapLevelCollectionsViewController;
+        private static AnnotatedBeatmapLevelCollectionsTableView _annotatedBeatmapLevelCollectionsTableView;
+        private static TableView _tableView;
+        private static GameObject _content;
+        private static List<GameObject> _disabledCells;
+
+        public const int MAX_PLAYLISTS_PER_PAGE = 6;
 
         public static void ButtonsInit()
         {
+            _content = GameObject.FindObjectOfType<AnnotatedBeatmapLevelCollectionsViewController>().transform.Find("TableView").transform.Find("Viewport").transform.Find("Content").gameObject;
+
             GameObject leftPlaylistsButtonObject = new GameObject("LeftPlaylistsButton");
             leftPlaylistsButtonObject.transform.parent = GameObject.FindObjectOfType<AnnotatedBeatmapLevelCollectionsViewController>().transform.Find("TableView").transform.Find("Viewport").transform;
             leftPlaylistsButtonObject.AddComponent<CanvasRenderer>();
             leftPlaylistsButtonObject.AddComponent<Touchable>();
-            leftPlaylistsButton = leftPlaylistsButtonObject.AddComponent<NoTransitionsButton>();
+            _leftPlaylistsButton = leftPlaylistsButtonObject.AddComponent<NoTransitionsButton>();
             leftPlaylistsButtonObject.transform.localPosition = new Vector3(-59, 0, 0);
             leftPlaylistsButtonObject.transform.localScale = new Vector3(0.2f, 1.5f, 1.5f);
-            leftPlaylistsButton.selectionStateDidChangeEvent += LeftPlaylistsButton_selectionStateDidChangeEvent;
+            leftPlaylistsButtonObject.layer = 5;
+            _leftPlaylistsButton.selectionStateDidChangeEvent += LeftPlaylistsButton_selectionStateDidChangeEvent;
             GameObject icon = GameObject.Instantiate(GameObject.Find("BackButton").transform.Find("Icon").gameObject, leftPlaylistsButtonObject.transform, false);
             icon.transform.localScale = new Vector3(6, 1, 1);
             icon.transform.localPosition = new Vector3(40, 0, 0);
@@ -31,26 +42,80 @@ namespace PlaylistLoaderLite.Objects
             rightPlaylistsButtonObject.transform.parent = GameObject.FindObjectOfType<AnnotatedBeatmapLevelCollectionsViewController>().transform.Find("TableView").transform.Find("Viewport").transform;
             rightPlaylistsButtonObject.AddComponent<CanvasRenderer>();
             rightPlaylistsButtonObject.AddComponent<Touchable>();
-            rightPlaylistsButton = rightPlaylistsButtonObject.AddComponent<NoTransitionsButton>();
+            _rightPlaylistsButton = rightPlaylistsButtonObject.AddComponent<NoTransitionsButton>();
             rightPlaylistsButtonObject.transform.localPosition = new Vector3(30, 0, 0);
             rightPlaylistsButtonObject.transform.localScale = new Vector3(0.05f, 1.5f, 1.5f);
-            rightPlaylistsButton.selectionStateDidChangeEvent += RightPlaylistsButton_selectionStateDidChangeEvent;
+            rightPlaylistsButtonObject.layer = 5;
+            _rightPlaylistsButton.selectionStateDidChangeEvent += RightPlaylistsButton_selectionStateDidChangeEvent;
             GameObject icon2 = GameObject.Instantiate(GameObject.Find("BackButton").transform.Find("Icon").gameObject, rightPlaylistsButtonObject.transform, false);
             icon2.transform.localScale = new Vector3(24, 1, 1);
             icon2.transform.localPosition = new Vector3(35, 0, 0);
             icon2.transform.localRotation = Quaternion.Euler(0, 0, 180);
+
+            _annotatedBeatmapLevelCollectionsViewController = GameObject.FindObjectOfType<AnnotatedBeatmapLevelCollectionsViewController>();
+            _annotatedBeatmapLevelCollectionsTableView = _annotatedBeatmapLevelCollectionsViewController.transform.Find("TableView").GetComponent<AnnotatedBeatmapLevelCollectionsTableView>();
+            _tableView = _annotatedBeatmapLevelCollectionsTableView.GetComponent<TableView>();
+
+            PostHandleOverflow();
         }
 
-        private static void RightPlaylistsButton_selectionStateDidChangeEvent(NoTransitionsButton.SelectionState obj)
+        public static void EnableButtons()
         {
-            if (obj == NoTransitionsButton.SelectionState.Pressed)
-                Plugin.Log.Critical("Pressed right!");
+            if (_leftPlaylistsButton != null)
+            {
+                _leftPlaylistsButton.gameObject.SetActive(true);
+                _rightPlaylistsButton.gameObject.SetActive(true);
+            }
+            else
+                ButtonsInit();
         }
 
-        private static void LeftPlaylistsButton_selectionStateDidChangeEvent(NoTransitionsButton.SelectionState obj)
+        public static void DisableButtons()
         {
-            if (obj == NoTransitionsButton.SelectionState.Pressed)
-                Plugin.Log.Critical("Pressed left!");
+            if (_leftPlaylistsButton != null)
+            {
+                _leftPlaylistsButton.gameObject.SetActive(false);
+                _rightPlaylistsButton.gameObject.SetActive(false);
+            }
+        }
+
+        private static void PreHandleOverFlow()
+        {
+            foreach (GameObject cell in _disabledCells)
+                cell.SetActive(true);
+        }
+
+        private static void PostHandleOverflow()
+        {
+            _disabledCells = new List<GameObject>();
+            foreach (Transform child in _content.transform)
+            {
+                if (child.localPosition.x >= MAX_PLAYLISTS_PER_PAGE * 13 && child.gameObject.activeSelf)
+                {
+                    child.gameObject.SetActive(false);
+                    _disabledCells.Add(child.gameObject);
+                }
+            }
+        }
+
+        private static void RightPlaylistsButton_selectionStateDidChangeEvent(NoTransitionsButton.SelectionState state)
+        {
+            if (state == NoTransitionsButton.SelectionState.Pressed)
+            {
+                PreHandleOverFlow();
+                _tableView.ScrollToCellWithIdx(8, TableViewScroller.ScrollPositionType.Beginning, true);
+                PostHandleOverflow();
+            }
+        }
+
+        private static void LeftPlaylistsButton_selectionStateDidChangeEvent(NoTransitionsButton.SelectionState state)
+        {
+            if (state == NoTransitionsButton.SelectionState.Pressed)
+            {
+                PreHandleOverFlow(); 
+                _tableView.ScrollToCellWithIdx(0, TableViewScroller.ScrollPositionType.Beginning, true);
+                PostHandleOverflow();
+            }
         }
     }
 }
